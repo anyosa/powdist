2e4bfaed6fec81473efa56e4e236413420cf95e6
Susan Anyosa <susanaliciach@gmail.com>
2017-07-08 17:04:07 -0300

    0.1.1

---
+++ R/powdist.R
@@ -0,0 +1,6 @@
+#' Density, distribution function, quantile function and random generation for the family of power and reversal power distributions..
+#'
+#' @author Susan A. C. Anyosa <susanch@usp.br>
+#' @docType package
+#' @name powdist
+NULL
