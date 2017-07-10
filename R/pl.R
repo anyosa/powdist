d9e3d9402dd00f0a95e8f567d4a12e326ad3f4eb
Susan Anyosa <susanaliciach@gmail.com>
2017-07-09 21:41:20 -0300

    test

--- R/pl.R
+++ R/pl.R
@@ -2,22 +2,19 @@
 #'
 #' Density function for the power logistic distribution with parameters mu, sigma and lambda.
 #'
-#' @export
-#'
 #' @param x vector of quantiles.
 #' @param lambda shape parameter.
 #' @param mu,sigma location and scale parameters.
 #' @references Bazán, J. L., Torres‐Avilés, F., Suzuki, A. K., & Louzada, F. (2017). Power and reversal power links for binary regressions: An application for motor insurance policyholders. Applied Stochastic Models in Business and Industry, 33(1), 22-34.
 #' @examples
 #' dpl(1, 1, 3, 4)
+#' @export
 dpl <- function(x, lambda, mu = 0, sigma = 1){
   d = (lambda/sigma) * dlogis((x-mu)/sigma) * (plogis((x-mu)/sigma)**(lambda-1))
   return(d)
 }
 
 #' Power logistic distribution
-#'
-#' @export
 #'
 #' Distribution function for the power logistic distribution with parameters mu, sigma and lambda.
 #' @param q vector of quantiles.
