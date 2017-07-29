e43f4fe26a0cf66741932940156749c8d74b5a7c
Susan Anyosa <susanaliciach@gmail.com>
2017-07-29 19:39:09 +0100

    imp gumbel fix

--- R/dpgmiv.R
+++ R/dpgmiv.R
@@ -10,6 +10,7 @@
 #' @param mu,sigma location and scale parameters.
 #' @references Bazán, J. L., Torres‐Avilés, F., Suzuki, A. K., & Louzada, F. (2017). Power and reversal power links for binary regressions: An application for motor insurance policyholders. \emph{Applied Stochastic Models in Business and Industry}, \strong{33}(1), 22-34.
 #' @references Anyosa, S. A. C. (2017). \emph{Binary regression using power and reversal power links}. (Master's thesis).
+#' @import VGAM
 #' @examples
 #' dpgmiv(1, 1, 3, 4)
 #' @export
