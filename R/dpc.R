6d16846c99c13640ded6071a4b24ed9611dd80c7
Susan Anyosa <susanaliciach@gmail.com>
2017-08-01 21:03:38 +0100

    ref ok

--- R/dpc.R
+++ R/dpc.R
@@ -8,7 +8,7 @@
 #' @param n number of observations.
 #' @param lambda shape parameter.
 #' @param mu,sigma location and scale parameters.
-#' @references Anyosa, S. A. C. (2017) \emph{Binary regression using power and reversal power links}. Master's thesis in portuguese. Interinstitutional Graduate Program in Statistics. Universidade de São Paulo - Universidade Federal de São Carlos.
+#' @references Anyosa, S. A. C. (2017) \emph{Binary regression using power and reversal power links}. Master's thesis in Portuguese. Interinstitutional Graduate Program in Statistics. Universidade de São Paulo - Universidade Federal de São Carlos. Available in \url{http://conteudo.icmc.usp.br/pessoas/jlbazan/download/thesis.pdf}.
 #' @references Bazán, J. L., Torres‐Avilés, F., Suzuki, A. K. and Louzada, F. (2017) Power and reversal power links for binary regressions: An application for motor insurance policyholders. \emph{Applied Stochastic Models in Business and Industry}, \strong{33}(1), 22-34.
 #' @examples
 #' dpc(1, 1, 3, 4)
