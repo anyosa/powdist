bfcfb648ef54e4d61abf334d45ac05be89669f67
Susan Anyosa <susanaliciach@gmail.com>
2017-08-25 00:06:53 +0100

    new dist

---
+++ R/dplaplace.R
@@ -0,0 +1,33 @@
+#' @title The Power Laplace Distribution
+#' @name plaplace
+#' @description Density, distribution function,
+#' quantile function and random generation for
+#' the power Laplace distribution with parameters mu, sigma and lambda.
+#' @param x,q vector of quantiles.
+#' @param p vector of probabilities.
+#' @param n number of observations.
+#' @param lambda shape parameter.
+#' @param mu,sigma location and scale parameters.
+#' @param log,log.p logical; if TRUE, probabilities p are given as log(p).
+#' @param lower.tail logical; if TRUE (default), probabilities are \eqn{P[X \le x ]}, otherwise, P[X > x].
+#' @importFrom stats runif
+#' @importFrom rmutil dlaplace
+#' @importFrom rmutil plaplace
+#' @importFrom rmutil qlaplace
+#' @details The power Laplace distribution has density
+#'
+#' \eqn{f(x)=[\lambda/\sigma][f((x-\mu)/\sigma)][F((x-\mu)/\sigma)] ^(\lambda-1)},
+#'
+#' where \eqn{-\infty<\mu<\infty} is the location paramether, \eqn{\sigma^2>0} the scale parameter and \eqn{\lambda>0} the shape parameter.
+#'
+#' @examples
+#' dplaplace(1, 1, 3, 4)
+#' @export
+dplaplace <- function(x, lambda = 1, mu = 0, sigma = 1, log = FALSE){
+  d = (lambda/sigma) * dlaplace((x-mu)/sigma) * (plaplace((x-mu)/sigma)**(lambda-1))
+  if (log == TRUE) {
+    d = log( (lambda/sigma) * dlaplace((x-mu)/sigma) * (plaplace((x-mu)/sigma)**(lambda-1)) )
+  }
+return(d)
+}
+
---
+++ R/qpt.R
@@ -0,0 +1,14 @@
+#' @name pt
+#' @examples
+#' qpt(0.2, 1, 3, 4, 1)
+#' @export
+qpt <- function(p, lambda = 1, mu = 0, sigma = 1, df, lower.tail = TRUE, log.p = FALSE){
+  if (lower.tail == TRUE & log.p == FALSE) {
+    q = qlaplace(p**(1/lambda), df)* sigma + mu
+  } else if (lower.tail == FALSE & log.p == FALSE) {
+    q = qlaplace((1-p)**(1/lambda), df)* sigma + mu
+  } else if (lower.tail == TRUE & log.p == TRUE) {
+    q = qlaplace(exp(p)**(1/lambda), df)* sigma + mu
+  } else {q = qlaplace((1-exp(p))**(1/lambda), df)* sigma + mu}
+  return(q)
+}
